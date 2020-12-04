f8b14ab6d7032cb491097e382c42af8ecb0d1030
Mrinal Bedi <mrinalbedi42@gmail.com>
2020-12-04 10:44:25 -0500

    project upload

---
+++ Sail/Sail/Controllers/MemberController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sail.Models;
+
+namespace Sail.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly SailContext _context;
+
+        public MemberController(SailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Member
+        public async Task<IActionResult> Index()
+        {
+            var sailContext = _context.Member.Include(m => m.ProvinceCodeNavigation);
+            return View(await sailContext.ToListAsync());
+        }
+
+        // GET: Member/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Member
+                .Include(m => m.ProvinceCodeNavigation)
+                .FirstOrDefaultAsync(m => m.MemberId == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return View(member);
+        }
+
+        // GET: Member/Create
+        public IActionResult Create()
+        {
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode");
+            return View();
+        }
+
+        // POST: Member/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MemberId,FullName,FirstName,LastName,SpouseFirstName,SpouseLastName,Street,City,ProvinceCode,PostalCode,HomePhone,Email,YearJoined,Comment,TaskExempt,UseCanadaPost")] Member member)
+        {
+            try
+            {
+                
+                if (ModelState.IsValid)
+                {
+                    
+                    _context.Add(member);
+                    await _context.SaveChangesAsync();
+                    TempData["message"] = "New Member record has been created Successfully";
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", e.GetBaseException().Message);
+                TempData["message"] = "Error occurred while creating a new member record " + e.GetBaseException().Message;
+                
+            }
+            
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", member.ProvinceCode);
+            return View(member);
+        }
+
+        // GET: Member/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Member.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProvinceCode"] = new SelectList(_context.Province.OrderBy(m=>m.Name), "ProvinceCode", "Name", member.ProvinceCode);
+            return View(member);
+        }
+
+        // POST: Member/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MemberId,FullName,FirstName,LastName,SpouseFirstName,SpouseLastName,Street,City,ProvinceCode,PostalCode,HomePhone,Email,YearJoined,Comment,TaskExempt,UseCanadaPost")] Member member)
+        {
+            if (id != member.MemberId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(member);
+                    await _context.SaveChangesAsync();
+                    TempData["message"] = "The existing member record has been updated successfully";
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MemberExists(member.MemberId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                catch(Exception e)
+                {
+                    ModelState.AddModelError("", e.GetBaseException().Message);
+                    TempData["message"] = "Error occurred while Updating the member record " + e.GetBaseException().Message;
+                }
+                //return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", member.ProvinceCode);
+            return View(member);
+        }
+
+        // GET: Member/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Member
+                .Include(m => m.ProvinceCodeNavigation)
+                .FirstOrDefaultAsync(m => m.MemberId == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return View(member);
+        }
+
+        // POST: Member/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            try
+            {
+                var member = await _context.Member.FindAsync(id);
+                _context.Member.Remove(member);
+                await _context.SaveChangesAsync();
+                TempData["message"] = "The Memeber record that you requested has been deleted successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+
+                ModelState.AddModelError("", e.GetBaseException().Message);
+                TempData["message"] = "Error occurred while Deleting the requested member record " + e.GetBaseException().Message; ;
+            }
+            return View("Delete");
+           
+        }
+
+        private bool MemberExists(int id)
+        {
+            return _context.Member.Any(e => e.MemberId == id);
+        }
+    }
+}
---
+++ Sail/Sail/Models/MetaData Classes/MemberMetaData.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace Sail.Models
+{
+    [ModelMetadataType(typeof(MemberMetaData))]
+    public partial class Member:IValidatableObject
+    {
+        SailContext _context = new SailContext();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            //if (MemberId != 0)
+            //{
+            //    FullName = FirstName + ' ' + LastName + " & " + SpouseFirstName;
+            //}
+            if(ProvinceCode!=null)
+            {
+                if (ProvinceCode.Length != 2)
+                {
+                    yield return new ValidationResult("The Province Code should be exactly 2 characters long", new[] { "ProvinceCode" });
+                }
+                else
+                {
+                    var province = _context.Province.Where(m => m.ProvinceCode == ProvinceCode).FirstOrDefault();
+                    if (province==null)
+                    {
+                        yield return new ValidationResult("The Province Code is not valid", new[] { "ProvinceCode" });
+                    }
+                    else
+                    {
+                        ProvinceCode = ProvinceCode.Trim().ToUpper();
+                    }
+                }     
+            }
+            Regex PostalCodePattern = new Regex((@"^[a-zA-Z]{1}[0-9]{1}[a-zA-Z]{1}\s{0,1}[0-9]{1}[a-zA-Z]{1}[0-9]{1}"),RegexOptions.IgnoreCase);
+            if (PostalCodePattern.IsMatch(PostalCode.Trim()))
+            {
+                if (!PostalCode.Contains(" "))
+                {
+                    PostalCode = PostalCode.Insert(3, " ");
+                    PostalCode = PostalCode.Trim().ToUpper();
+                }
+                else
+                    PostalCode = PostalCode.Trim().ToUpper();   
+            }
+            else
+            {
+                yield return new ValidationResult("The Postal Code entered is not in valid canadian format", new[] { "PostalCode" });
+            }
+
+   
+            Regex HomePhonePattern = new Regex(@"^\d\d\d-{0,1}\d\d\d-{0,1}\d\d\d\d");
+            if (HomePhone != null)
+            {
+                if (HomePhonePattern.IsMatch(HomePhone))
+                {
+                    if (!HomePhone.Contains('-'))
+                    {
+                        HomePhone = HomePhone.Insert(3, "-");
+                        HomePhone = HomePhone.Insert(7, "-");
+                        HomePhone = HomePhone.Trim();
+                    }
+                }
+                else
+                {
+                    yield return new ValidationResult("The home Phone number entered is not in valid format 999-999-9999", new[] { "HomePhone" });
+                }
+            }
+
+            if(string.IsNullOrEmpty(SpouseFirstName) && string.IsNullOrEmpty(SpouseLastName))
+            {
+                FullName = LastName.Trim() + "," + FirstName.Trim();
+            }
+            else
+            {
+                if(SpouseLastName==null || SpouseLastName==LastName)
+                {
+                    FullName = FirstName.Trim() + ' ' + LastName.Trim() + " & " + SpouseFirstName.Trim();
+                }
+                else
+                {
+                    FullName = LastName.Trim() + "," + FirstName.Trim() + " & " + SpouseLastName.Trim() + "," + SpouseFirstName.Trim();
+                }
+            }
+
+            if(UseCanadaPost)
+            {
+                if(string.IsNullOrEmpty( Street) && string.IsNullOrEmpty(City))
+                {
+                    yield return new ValidationResult("The Street name and City Name field are required fields if you have checked Canada Post checkbox", new[] { "Street" });
+                }
+            }
+            else
+            {
+                if(string.IsNullOrEmpty(Email))
+                {
+                    yield return new ValidationResult("The Email address field is required", new[] { "Email" });
+                }
+            }
+            if (MemberId == 0)
+            {
+                var duplicateID = _context.Member.Where(x => x.MemberId == MemberId).FirstOrDefault();
+                if (duplicateID != null)
+                {
+                    yield return new ValidationResult("The Member Id entered is already on file", new[] { "MemberId" });
+                }
+            }
+            if (Street!=null)
+            {
+                Street = Street.Trim();
+            }
+            if(City!=null)
+            {
+                City = City.Trim();
+            }
+            if(Email!=null)
+            {
+                Email = Email.Trim();
+            }    
+            if(Comment!=null)
+            {
+                Comment = Comment.Trim();
+            }
+            yield return ValidationResult.Success;
+        }
+    }
+    public class MemberMetaData
+    {
+        public int MemberId { get; set; }
+
+        
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "The First Name field is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The Last Name field is required")]
+        public string LastName { get; set; }
+        public string SpouseFirstName { get; set; }
+        public string SpouseLastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+
+        [Required(ErrorMessage ="The province code field is required")]
+        
+        public string ProvinceCode { get; set; }
+        public string PostalCode { get; set; }
+        public string HomePhone { get; set; }
+        public string Email { get; set; }
+        public int? YearJoined { get; set; }
+        public string Comment { get; set; }
+        public bool TaskExempt { get; set; }
+        public bool UseCanadaPost { get; set; }
+    }
+}
